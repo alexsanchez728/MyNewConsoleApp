60838320b6f4b18f500f9e8aebf142b6cd5c7c3f
Alex Sanchez <alesanchez728@gmail.com>
2018-01-09 21:09:02 -0600

    learning C#

--- MyNewConsoleApp/Program.cs
+++ MyNewConsoleApp/Program.cs
@@ -33,8 +33,36 @@ namespace MyNewConsoleApp
               then provide comma-delimited list of variables to
               use in the placeholders.
             */
+
             Console.WriteLine($"{firstName} {lastName} purchased on {purchaseDate}");
             Console.ReadLine();
+
+            /*
+                Not only do you have to say what type the variable is, you also
+                have to instantiate that exact same type of object on assignment.
+                This may seem redundant, but it's part of the C# language compiler's
+                type checking constraints.
+            */
+            string[] products = new string[] {
+                "Motorcycle",
+                "Sofa",
+                "Sandals",
+                "Omega Watch",
+                "iPhone"
+            };
+
+            /*
+                This syntax should look very similar to what you did in an Angular
+                partial's ng-repeat directive. However, once again you have to
+                explicitly say what type of variable product is. Since the products
+                array holds strings, then its type must be string.
+            */
+
+            foreach (string product in products)
+            {
+                Console.WriteLine(product);
+            }
+                Console.ReadLine();
         }
     }
 }
